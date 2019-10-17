da251b872012103068e32924d9b59b8db18ea050
Erika Szabo <erika.szabo.geo@gmail.com>
2019-10-17 14:51:20 -0400

    total assigment duration in a specific cohort

---
+++ 2_queries_joins/2_total_time_assignment_cohort.sql
@@ -0,0 +1,11 @@
+SELECT SUM(duration)
+  FROM assignment_submissions
+  JOIN students
+  ON assignment_submissions.student_id = students.id
+  WHERE students.id IN (
+    SELECT students.id
+      FROM students
+      JOIN cohorts
+      ON students.cohort_id = cohorts.id
+      WHERE cohorts.name = 'FEB12'
+    );
