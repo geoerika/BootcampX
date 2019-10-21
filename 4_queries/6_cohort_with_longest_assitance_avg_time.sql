fa153becf269c518bd98f0fa0ace3fa4b76a904f
Erika Szabo <erika.szabo.geo@gmail.com>
2019-10-21 11:28:54 -0400

    cohort with highest average assistance time

---
+++ 4_queries/6_cohort_with_longest_assitance_avg_time.sql
@@ -0,0 +1,16 @@
+SELECT cohorts.name, AVG(completed_at - started_at) as max_assistance_time
+  FROM assistance_requests
+  JOIN students
+  ON student_id = students.id
+  JOIN cohorts
+  ON cohorts.id = students.cohort_id
+  GROUP BY cohorts.name
+  WHERE max_assistance_time = MAX(
+    SELECT AVG(completed_at - started_at) as average_cohort_assistance_time
+    FROM assistance_requests
+    JOIN students
+    ON student_id = students.id
+    JOIN cohorts
+    ON cohorts.id = students.cohort_id
+    GROUP BY cohorts.name
+    ORDER BY average_cohort_assistance_time);
