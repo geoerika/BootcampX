14bef63fac5d3a4e2012e4755a0024f009eead77
Erika Szabo <erika.szabo.geo@gmail.com>
2019-10-21 11:26:50 -0400

    assistance requests per student

---
+++ 4_queries/2_student_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(*)
+  FROM assistance_requests
+  JOIN students
+  ON assistance_requests.student_id = students.id
+  WHERE students.name = 'Elliot Dickinson'
+  GROUP BY students.name;
