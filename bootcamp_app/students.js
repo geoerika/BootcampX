5beb6d2eea24cc0e86d7b4c81003be5577c9e630
Erika Szabo <erika.szabo.geo@gmail.com>
2019-11-08 15:39:50 -0500

    dsiplayed students name, ids and cohort name

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,19 +7,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-
-
 pool.query(`
-    SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-    FROM students
-    JOIN cohorts
-    ON students.cohort_id = cohorts.id
-    WHERE cohorts.name LIKE '%${process.argv[2]}%'
-    LIMIT ${process.argv[3] || 5};
-  `)
-  .then(res => {
-    res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohorts} cohort`);
-    })
+  SELECT students.id as id, students.name as name, cohorts.name as cohort
+  FROM students
+  INNER JOIN cohorts
+  ON students.cohort_id = cohorts.id
+  LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
   })
-  .catch(err => console.err('query error', err.stack));
+});
