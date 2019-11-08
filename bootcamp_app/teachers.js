6263ce31ec263430fa9d4ee2fe0c812284a155a3
Erika Szabo <erika.szabo.geo@gmail.com>
2019-11-08 16:48:49 -0500

    parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,7 +7,10 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`];
+const queryString = (`
     SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
     FROM teachers
     JOIN assistance_requests
@@ -16,9 +19,11 @@ pool.query(`
     ON students.id = assistance_requests.student_id
     JOIN cohorts
     ON students.cohort_id = cohorts.id
-    WHERE cohorts.name LIKE '%${process.argv[2]}%'
+    WHERE cohorts.name LIKE $1
     ORDER BY teachers.name;
-  `)
+  `);
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(row => {
       console.log(`${row.cohort}: ${row.teacher}`);
